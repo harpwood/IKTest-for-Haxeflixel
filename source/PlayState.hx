936662de1a7350a9b84eb38d7953c08314f99d52
harpwood <harpwood@gmail.com>
2022-01-13 16:18:22 +0200

    Replaced arm() with IK_limb()
    Now the limb is customable via code

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -8,26 +8,50 @@ import objects.Line;
 
 class PlayState extends FlxState
 {
+	var upperLeg1:Line = new Line();
+	var lowerLeg1:Line = new Line();
+	var l1:Float = 100;
+	var l2:Float = 100;
 	var x1:Float = FlxG.width / 2;
 	var y1:Float = FlxG.height / 2;
-	var line:Line = new Line();
-	var line2:Line = new Line();
+	var sign:Int = 1;
 
 	override public function create()
 	{
 		super.create();
-		add(line);
-		add(line2);
+		add(upperLeg1);
+		add(lowerLeg1);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
-		arm();
+		if (FlxG.mouse.justPressed)
+			sign = -sign;
+
+		var limb1:Limb = {
+			limb: upperLeg1,
+			lenght: l1,
+			X: x1,
+			Y: y1
+		}
+		var limb2:Limb = {
+			limb: lowerLeg1,
+			lenght: l2,
+			X: FlxG.mouse.screenX,
+			Y: FlxG.mouse.screenY
+		}
+
+		IK_limb(limb1, limb2, sign);
 	}
 
-	function arm()
+	/**
+	 * @param   limb1			 The first part of the limb.
+	 * @param   limb2		     The second part of the limb.
+	 * @param   sign		     Affects limb's destination by passing a positive or a negative Int
+	 */
+	function IK_limb(limb1:Limb, limb2:Limb, sign:Int):Void
 	{
 		//		A
 		//		|\
@@ -46,27 +70,27 @@ class PlayState extends FlxState
 		// c: the distance between A and B
 
 		// bones length
-		var b = 120;
-		var a = 120;
+		var b = limb1.lenght;
+		var a = limb2.lenght;
 		// the anchor point
-		var Ax = x1;
-		var Ay = y1;
+		var Ax = limb1.X;
+		var Ay = limb1.Y;
 		// the mouse point
-		var targetx = FlxG.mouse.screenX;
-		var targety = FlxG.mouse.screenY;
+		var ax = limb2.X;
+		var ay = limb2.Y;
 
 		// the distance between point A and B
-		var c = Math.min(Math.round(FlxMath.vectorLength(Ax - targetx, Ay - targety) + (b / 12)), a + b);
+		var c = Math.min(Math.round(FlxMath.vectorLength(Ax - ax, Ay - ay) + (b / 12)), a + b);
 
 		// The angle of A
-		var alpha = Math.round(FlxAngle.asDegrees(Math.atan2(targety - Ay, targetx - Ax)));
+		var alpha = Math.round(FlxAngle.asDegrees(Math.atan2(ay - Ay, ax - Ax)));
 
 		// The position of point B
 		var Bx = Ax + c * Math.cos(FlxAngle.asRadians(alpha));
 		var By = Ay + c * Math.sin(FlxAngle.asRadians(alpha));
 
 		// The angle of point C
-		var beta = FlxAngle.asDegrees(Math.acos(Math.min(1, Math.max(-1, b * b + c * c - a * a) / (2 * b * c))));
+		var beta = FlxMath.signOf(sign) * FlxAngle.asDegrees(Math.acos(Math.min(1, Math.max(-1, b * b + c * c - a * a) / (2 * b * c))));
 		var gama = alpha - beta;
 
 		// The position of point C
@@ -74,7 +98,22 @@ class PlayState extends FlxState
 		var Cy = Ay + b * Math.sin(FlxAngle.asRadians(gama));
 
 		// draw it!
-		line.drawIt(Ax, Ay, Cx, Cy);
-		line2.drawIt(Cx, Cy, Bx, By);
+
+		cast(limb1.limb, Line).drawIt(Ax, Ay, Cx, Cy);
+		cast(limb2.limb, Line).drawIt(Cx, Cy, Bx, By);
 	}
+}
+
+/**
+ * @param limb	The Line to be drawn
+ * @param lengh The lenght of the line
+ * @param X		The X(n) position of the line. If this limb is the first n = 1 (aka X1) else n = 2 (aka X2)
+ * @param Y		The Y(n) position of the line. If this limb is the first n = 1 (aka Y1) else n = 2 (aka Y2)
+ */
+typedef Limb =
+{
+	var limb:Line;
+	var lenght:Float;
+	var X:Float;
+	var Y:Float;
 }
